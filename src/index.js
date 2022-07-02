36b90995e403bcb0df6bc766352c8962a72768c2
Lázaro <lazaroyaesta@gmail.com>
2022-07-02 16:21:50 +0200

    🩹 Fix server initialization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
+// CommonJs
 import Fastify from 'fastify';
 
 const fastify = Fastify({
     logger: true,
 });
 
-const start = async () => {
-    await fastify.listen(3000);
+fastify.get('/', (request, reply) => {
+    reply.send({ hello: 'world' });
+});
 
-    fastify.log.info(`Server listening on ${fastify.server.addres().port}`);
+/**
+ * Run the server!
+ */
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000 });
+
+        fastify.log.info(
+            `Server listening on ${fastify.server.address().port}`
+        );
+    } catch (err) {
+        console.log(`--- ERROR CATCH ---`);
+        fastify.log.error(err);
+        process.exit(1);
+    }
 };
 
 start();
